6dd0feaef2cbbfb3cf82746aa028962b67dccf96
Afnan Enayet <afnan.enayet@gmail.com>
2017-07-15 15:36:41 -0400

    Created skeleton for initial python file

---
+++ train_eval.py
@@ -0,0 +1,11 @@
+# This trains and evaluates the Titanic model for the Kaggle DS Titanic 
+# competition
+
+def main():
+    """Wrapper for main function"""
+    pass
+
+# Wrapper for main function
+if __name__="__main__":
+    main()
+
